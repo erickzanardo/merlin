bb3a4828de54f2dec406b203dff3d7b30256d73e
Erick <erickzanardoo@gmail.com>
2024-02-15 13:55:39 -0300

    other fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
 }
